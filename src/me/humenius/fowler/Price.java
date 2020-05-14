9866ebe44d35ec49fa90b63f38e1c1346f3b02fb
Humenius <contact@humenius.me>
2020-05-14 14:12:08 +0200

    Rewrite Price interface to abstract

--- src/me/humenius/fowler/Price.java
+++ src/me/humenius/fowler/Price.java
@@ -1,30 +1,33 @@
 package me.humenius.fowler;
 
-public interface Price {
-    double getCharge();
+public abstract class Price {
+    abstract int getPriceCode();
 
-    int getFrequentRenterPoints();
+    abstract double getCharge(int daysRented);
 
-    static double amountFor(Rental rental) {
-        double total = 0;
-        switch (rental.getMovie()
-                      .getPriceCode()) {
-        case REGULAR:
-            total += 2;
-            if (rental.getDaysRented() > 2) {
-                total += (rental.getDaysRented() - 2) * 1.5;
-            }
-            break;
-        case NEW_RELEASE:
-            total += rental.getDaysRented() * 3;
-            break;
-        case CHILDREN:
-            total += 1.5;
-            if (rental.getDaysRented() > 3) {
-                total += (rental.getDaysRented() - 3) * 1.5;
-            }
-            break;
-        }
-        return total;
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
     }
+//
+//    static double amountFor(Rental rental) {
+//        double total = 0;
+//        switch (rental.getMovie().getPriceCode()) {
+//        case REGULAR:
+//            total += 2;
+//            if (rental.getDaysRented() > 2) {
+//                total += (rental.getDaysRented() - 2) * 1.5;
+//            }
+//            break;
+//        case NEW_RELEASE:
+//            total += rental.getDaysRented() * 3;
+//            break;
+//        case CHILDREN:
+//            total += 1.5;
+//            if (rental.getDaysRented() > 3) {
+//                total += (rental.getDaysRented() - 3) * 1.5;
+//            }
+//            break;
+//        }
+//        return total;
+//    }
 }
