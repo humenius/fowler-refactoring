d2a9fb22fc9530642718579c64d74789713e0652
Humenius <contact@humenius.me>
2020-05-15 09:24:59 +0200

    Add PriceTest and RentalTest

---
+++ src/test/java/me/humenius/fowler/prices/PriceTest.java
@@ -0,0 +1,37 @@
+package me.humenius.fowler.prices;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+public class PriceTest {
+    @Test
+    public void testChildrenPriceCalculation() {
+        Price price = new ChildrenPrice();
+        int daysRented = 5;
+
+        double actual = price.getCharge(daysRented);
+        double expected = 1.5 + (((double) daysRented) - 3) * 1.5;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testNewReleasePriceCalculation() {
+        Price price = new NewReleasePrice();
+        int daysRented = 5;
+
+        double actual = price.getCharge(daysRented);
+        double expected = (double) daysRented * 3;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRegularPriceCalculation() {
+        Price price = new RegularPrice();
+        int daysRented = 5;
+
+        double actual = price.getCharge(daysRented);
+        double expected = 2.0 + (((double) daysRented) - 2) * 1.5;
+        assertEquals(actual, expected);
+    }
+}
